9f0cafc11966b9d0d8c3693b2f0373006134ff4d
Joserbp <josera.jack@gmail.com>
2023-04-20 14:14:04 -0600

    CORS

--- PL_MVC/Global.asax.cs
+++ PL_MVC/Global.asax.cs
@@ -17,12 +17,5 @@ namespace PL_MVC
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
-            {
-                HttpContext.Current.Response.Flush();
-            }
-        }
     }
 }
